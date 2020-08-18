5896dfefc498bef361f8b11a8a27169ebe24f85a
rishik-m <rishik.makhija2000@gmail.com>
2020-08-19 04:32:24 +0530

    Updates

--- src/components/MinCard.js
+++ src/components/MinCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, View, Image, Dimensions } from 'react-native';
 
-const MiniCard = () => {
+const MiniCard = (props) => {
     return (
         <View style={{ flexDirection: 'row', margin: 10, marginBottom: 0 }}>
             <Image
                 source={{
-                    uri: 'https://images.unsplash.com/photo-1536431311719-398b6704d4cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'
+                    uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`
                 }}
                 style={{
                     width: '45%',
@@ -20,8 +20,8 @@ const MiniCard = () => {
                 }}
                     ellipsizeMode='tail'
                     numberOfLines={3}
-                > This is an incredible place </Text>
-                <Text style={{ fontSize: 12 }}> Carryminati </Text>
+                > {props.title} </Text>
+                <Text style={{ fontSize: 12 }}> {props.channel} </Text>
             </View>
         </View>
     );
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
-import { View, ScrollView, TextInput } from 'react-native';
+import { View, ScrollView, TextInput, FlatList, ActivityIndicator } from 'react-native';
 import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import MiniCard from '../components/MinCard';
 
+
+// https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=songs&type=video&key=AIzaSyDTKlsqNopd7PbJVqMEp-OELL3APAeiytc
+
 const SearchScreen = () => {
     const [value, setValue] = useState("")
+    const [miniCardData, setMiniCard] = useState([])
+    const [loading, setLoading] = useState(false)
+    const fetchData = () => {
+        setLoading(true)
+        fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${value}&type=video&key=AIzaSyDTKlsqNopd7PbJVqMEp-OELL3APAeiytc`)
+            .then(res => res.json())
+            .then(data => {
+                setLoading(false)
+                setMiniCard(data.items)
+            })
+    }
     return (
         <View style={{
             flex: 1,
@@ -23,18 +37,20 @@ const SearchScreen = () => {
                     onChangeText={text => setValue(text)}
                     style={{ width: '70%', backgroundColor: '#e6e6e6' }}
                 />
-                <MaterialCommunityIcons name="send" size={24} color="black" />
+                <MaterialCommunityIcons name="send" size={24} color="black" onPress={() => fetchData()} />
             </View>
-            <ScrollView>
-                <MiniCard />
-                <MiniCard />
-                <MiniCard />
-                <MiniCard />
-                <MiniCard />
-                <MiniCard />
-                <MiniCard />
-                <MiniCard />
-            </ScrollView>
+            {loading ? <ActivityIndicator style={{ marginTop: 10 }} size="large" color="red" /> : null}
+            <FlatList
+                data={miniCardData}
+                renderItem={({ item }) => {
+                    return <MiniCard
+                        videoId={item.id.videoId}
+                        title={item.snippet.title}
+                        channel={item.snippet.channelTitle}
+                    />
+                }}
+                keyExtractor={(item) => item.id.videoId}
+            />
         </View>
     );
 }
